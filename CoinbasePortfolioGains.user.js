993c920ff7b9e565dffd39651b6d64c8d0691df0
S. McCandlish <smccandlish@gmail.com>
2021-02-26 09:19:33 -0800

    Update CoinbasePortfolioGains.user.js
    * Made currency symbol easy to set.
    * Moved the display to top center, away from current portfolio value amount, and make it slightly smaller. I find this less visually confusing than having it immediately next to the portfolio value.
    * Made it work on the Home/dashboard page as well as the Portfolio/account page.

--- CoinbasePortfolioGains.user.js
+++ CoinbasePortfolioGains.user.js
@@ -1,22 +1,24 @@
 // ==UserScript==
 // @name         Coinbase Portfolio Gains
-// @version      1.1
-// @description  Shows Coinbase portfolio gains
-// @author       KevDuc
-// @namespace    https://kevduc.github.io/
-// @homepageURL  https://github.com/kevduc/userscripts
-// @downloadURL  https://raw.githubusercontent.com/kevduc/userscripts/master/CoinbasePortfolioGains.user.js
-// @updateURL    https://raw.githubusercontent.com/kevduc/userscripts/master/CoinbasePortfolioGains.user.js
-// @supportURL   https://github.com/kevduc/userscripts/issues
+// @version      1.1.1.2021-02-26
+// @description  Shows Coinbase portfolio gain/loss
+// @author       KevDuc; tweaked by SMcCandlish
+// @namespace    https://github.com/SMcCandlish/Coinbase_Gains_TamperMonkey_Script
+// @homepageURL  https://github.com/SMcCandlish/Coinbase_Gains_TamperMonkey_Script
+// @downloadURL  https://raw.githubusercontent.com/SMcCandlish/Coinbase_Gains_TamperMonkey_Script/master/CoinbasePortfolioGains.user.js
+// @updateURL    https://raw.githubusercontent.com/SMcCandlish/Coinbase_Gains_TamperMonkey_Script/master/CoinbasePortfolioGains.user.js
+// @supportURL   https://github.com/SMcCandlish/Coinbase_Gains_TamperMonkey_Script/issues
 // @match        https://www.coinbase.com/*
 // @grant        none
 // @run-at       document-end
 // ==/UserScript==
+// Based on original at: https://github.com/kevduc/userscripts
 
 (function() {
     'use strict';
 
-    const totalInvestment = 0; // Change this to the total amount you invested (in your local currency)
+    const totalInvestment = 1324.62; // Change this to the total amount you invested (in your local currency)
+    const currencySymbol = `$`; // Change this to whatever you need, like £.
 
     // Helper functions
     const pause = (milli) => new Promise((resolve, reject) => setTimeout(resolve, milli));
@@ -47,22 +49,22 @@
     const invested = totalInvestmentHistory[0] || 0;
 
     function updateROI(balance, roi) {
-        const value = parseFloat(balance.innerText.replace(/[$€£,]/g,''));
+        const value = parseFloat(balance.innerText.replace(/[$£€,]/g,''));
         const profit = value - invested;
         roi.style.color = profit > 0 ? 'green' : 'red';
-        roi.innerText = `${profit > 0 ? '+' : '-'}£${Math.abs(profit).toFixed(2)} (${(100*profit/invested).toFixed(2)}%)`;
+        roi.innerText = `${profit > 0 ? '+' : '-'}${currencySymbol}${Math.abs(profit).toFixed(2)} (${(100*profit/invested).toFixed(2)}%)`;
     }
 
     async function init() {
-        const balance = await document.querySelectorWhenLoaded('h1[class*="Balance__BalanceHeader"]');
+        const balance = await document.querySelectorWhenLoaded('h1[class*="Balance__BalanceHeader"], h1[class*="Balance__BalanceHeader-ek4zs2-3"]');
 
         const roi = document.createElement('h1');
         roi.className = balance.className;
-        roi.style = `display:inline-block; font-size:x-large`;
+        roi.style = `font-size: large; text-align: center;`;
         roi.id = "balanceROI-tampermonkey";
 
-        balance.style.display = 'inline-block';
-        balance.insertAdjacentElement('afterend', roi);
+        const periodSelector = await document.querySelectorWhenLoaded('div[class*="PeriodSelector__SelectorContainer-sc-1w75yzt-0"]');
+        periodSelector.insertAdjacentElement('beforebegin', roi);
 
         const balanceTextNode = balance.firstChild;
 
